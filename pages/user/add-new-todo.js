4b1b4a96ee552ef5278b3f25f25fe1b6ee1f5c08
Hizkias Abraham <hizkiasnew@gmail.com>
2022-04-08 17:31:32 +0300

    implement app layout and routes

---
+++ pages/user/add-new-todo.js
@@ -0,0 +1,7 @@
+import Layout from "../../components/layout";
+
+export default function AddNewTodo (){
+    return <Layout>
+        Add new todo
+    </Layout>
+}
---
+++ pages/user/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function UserHome() {
+    return (
+        <Layout>
+            This is the user home
+        </Layout>
+    )
+}
