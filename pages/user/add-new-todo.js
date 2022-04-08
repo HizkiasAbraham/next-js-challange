9212aa6e10837b405730f7944377e39fd4e63637
Hizkias Abraham <hizkiasnew@gmail.com>
2022-04-08 18:40:47 +0300

    add create new todo ui

--- pages/user/add-new-todo.js
+++ pages/user/add-new-todo.js
@@ -1,7 +1,51 @@
-import Layout from "../../components/layout";
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@mui/material"
+import { useState } from "react"
+import TodoDatePiker from "../../components/date-picker"
+import Layout from "../../components/layout"
 
-export default function AddNewTodo (){
-    return <Layout>
-        Add new todo
+export default function AddNewTodo() {
+  const [value, setValue] = useState(new Date("2014-08-18T21:11:54"))
+
+  const handleChange = (newValue) => {
+    setValue(newValue)
+  }
+  return (
+    <Layout>
+      <Typography variant="h5" component={"p"}>
+        Create Todo
+      </Typography>
+      <Box mt={3}>
+        <Paper>
+          <form>
+            <Grid container p={2}>
+              <Grid item xs={12} md={4}>
+                <TextField fullWidth label="Title" />
+              </Grid>
+            </Grid>
+            <Grid container p={2}>
+              <Grid item xs={12} md={6}>
+                <TextField fullWidth label="Description" />
+              </Grid>
+            </Grid>
+            <Grid container p={2}>
+              <Grid item xs={12} md={4}>
+                <TodoDatePiker label={" Pick Date"} />
+              </Grid>
+            </Grid>
+            <Box p={2}>
+              <Button variant="contained">Create</Button>
+            </Box>
+          </form>
+        </Paper>
+      </Box>
     </Layout>
+  )
 }
